a790d3dae06127a7d6ba3681de306fad47e369e2
dinhthanh143 <neonlimea@gmail.com>
2025-03-27 11:22:47 +0700

    a

--- ss36/js files/b4.js
+++ ss36/js files/b4.js
@@ -1,18 +1,19 @@
 let mode = document.getElementById("mode-icon");
 let body = document.getElementsByTagName("body")[0];
 let current = JSON.parse(localStorage.getItem("ss36_b4"))
-if(current === "dark"){
+if(current == "light"){
     body.classList.remove("dark-mode");
- }else if(current === "light"){
+}else{
     body.classList.add("dark-mode");
- }
-mode.onclick = function () {
- if(current === "light"){
-    body.classList.remove("dark-mode");
-    current = "dark"
- }else if(current === "dark"){
-    body.classList.add("dark-mode");
-    current = "light"
- }
- localStorage.setItem("ss36_b4", JSON.stringify(current));
-};
+}
+
+mode.onclick = function(){
+    if(current == "light"){
+        body.classList.add("dark-mode");
+        current = "dark"
+    }else{
+        body.classList.remove("dark-mode");
+        current = "light"
+    }
+    localStorage.setItem("ss36_b4", JSON.stringify(current))
+}
